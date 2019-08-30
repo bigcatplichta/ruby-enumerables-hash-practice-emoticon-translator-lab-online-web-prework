2cb6e8401b886fc3320d9773fd02ac3506a17d72
Matt Plichta <bigcatplichta@gmail.com>
2019-08-30 23:13:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ require "yaml"
 def load_library(library)
   emote_list = YAML.load_file(library)
   get_meaning = {}
+  get_emoticon = {}
   
   emote_list.each do | item |
    item.each do | meaning |
