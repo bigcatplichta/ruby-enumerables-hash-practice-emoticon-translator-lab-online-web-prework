042d38ca324766cd26097f0deaf38137e16c727d
Matt Plichta <bigcatplichta@gmail.com>
2019-08-30 23:59:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,8 +15,8 @@ def load_library(library)
     emotes = item[1]
     ja_emote = emotes[1]
     en_emote = emotes[0]
-    get_meaning[ja_emote] = meaning unless get_meaning[ja_emote]
-    get_emoticon[en_emote] = ja_emote unless get_emoticon[en_emote]
+    "get_meaning"[ja_emote] = meaning unless "get_meaning"[ja_emote]
+    "get_emoticon"[en_emote] = ja_emote unless "get_emoticon"[en_emote]
   
     
 
