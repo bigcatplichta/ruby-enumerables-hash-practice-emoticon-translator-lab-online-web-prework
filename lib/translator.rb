9717f73555e1e9ea81cb0f00c434f7825d6734b8
Matt Plichta <bigcatplichta@gmail.com>
2019-08-31 00:49:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def load_library(library)
     emotes = item[1]
     ja_emote = emotes[1]
     en_emote = emotes[0]
-    get_meaning[ja_emote] = meaning unless get_meaning[ja_emote]
+    translated_hash[:get_meaning][ja_emote] = meaning unless get_meaning[ja_emote]
     get_emoticon[en_emote] = ja_emote unless get_emoticon[en_emote]
     binding.pry
    end
