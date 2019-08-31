b89df8d5f459008bf434b85043aad3b91a24a539
Matt Plichta <bigcatplichta@gmail.com>
2019-08-31 00:05:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,20 +5,18 @@ require "yaml"
 
 def load_library(library)
   emote_list = YAML.load_file(library)
-  translated_hash = {
-  get_meaning = {},
-  "get_emoticon": {}
-  }
+  translated_hash = {}
+  get_meaning = {}
+  get_emoticon = {}
+  
   
   emote_list.each do | item |
    item.each do | meaning |
     emotes = item[1]
     ja_emote = emotes[1]
     en_emote = emotes[0]
-    get_meaning[ja_emote] = meaning unless get_meaning[ja_emote]
-    get_emoticon[en_emote] = ja_emote unless get_emoticon[en_emote]
-  
-    
+    translated_hash[get_meaning][ja_emote] = meaning unless translated_hash[get_meaning][ja_emote]
+    translated_hash[get_emoticon][en_emote] = ja_emote unless translated_hash[get_emoticon][en_emote]
 
    end
   end
